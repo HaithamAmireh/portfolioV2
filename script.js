44d190fb788083a1ce415c1c06b9ba30c49432e4
Haitham Amireh <haithamamireh@Haithams-MacBook-Pro.local>
2024-12-31 11:12:00 +0300

    edits

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const themeToggle = document.getElementById("theme-toggle");
-const icon = themeToggle.querySelector("i");
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("light-theme");
-    if (document.body.classList.contains("light-theme")) {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-    } else {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-    }
-});
+// const themeToggle = document.getElementById("theme-toggle");
+// const icon = themeToggle.querySelector("i");
+// themeToggle.addEventListener("click", () => {
+//     document.body.classList.toggle("light-theme");
+//     if (document.body.classList.contains("light-theme")) {
+//         icon.classList.remove("fa-sun");
+//         icon.classList.add("fa-moon");
+//     } else {
+//         icon.classList.remove("fa-moon");
+//         icon.classList.add("fa-sun");
+//     }
+// });
